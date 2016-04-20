8de7400f357c9d6d9b9660af7cbf6fcb4ca9f43e
larryhernandez <larryhernandez@users.noreply.github.com>
2016-04-20 09:39:26 -0500

    Update process_special_rate_codes.R

--- process_special_rate_codes.R
+++ process_special_rate_codes.R
@@ -1,9 +1,18 @@
 process_special_rate_codes <- function(rate_as_char){
+# DESCRIPTION: Maps the specialized input character variable rate_as_char into numeric variables using a switch() statement
+#              Returns -1 for inputs that are not identified by the mapping defined within the switch() statement.
+#
+# EXAMPLE:     process_special_rate_codes('LE20') returns 10
+#
+# INPUTS:
+#   rate_as_char    character variable that is converted to a pre-defined numerical value
 
-#  switch(rate_as_char, "LE1" = 1, "LE5"= 5, "LE10" = 10, "LE20" = 20, "LT50" = 50, "GE50" = 50, "GE80" = 80,
-#  "GE90" = 90, "GE95" = 95, "GE99" = 99, "PS" = 0, -1)
-  
+# LE:= Less than or equal to
+# GE:= Greater than or equal to
+# LT:= Less than or equal to
+# PS:= Privacy of Students maintained. Value reported as 0.
+
   switch(rate_as_char, "LE1" = 1, "LE5"= 2.5, "LE10" = 5, "LE20" = 10, "LT50" = 25, "GE50" = 75, "GE80" = 90,
   "GE90" = 95, "GE95" = 97, "GE99" = 99, "PS" = 0, -1)
-#   return(-1)
+
 }
